535e4b1f222e09e0f08ba6fc93ba3dbadefacfae
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-03-05 16:07:39 +0100

    Added unit tests for <App/> using Enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,7 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import rmwcTestPolyfill from 'rmwc/Base/testPolyfill';
+Enzyme.configure({ adapter: new Adapter() });
 
 //import and run this to fix the the MDC errors
 rmwcTestPolyfill();
