aff9519b1c384f69178a6b07d1f22a6b1b40c47f
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-01 19:21:47 +0200

    Added sass-lint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import 'material-components-web/material-components-web.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
