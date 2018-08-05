38279faa051040d7f99f595a0d7adbd3285e896e
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-05 19:13:46 +0200

    Added perspective and zoom sliders

--- src/Pages/TiltShiftGenerator.js
+++ src/Pages/TiltShiftGenerator.js
@@ -22,9 +22,13 @@ export default class TiltShiftGenerator extends Component {
       imageWidth: 0,
       imageHeight: 0,
       position: 0.5,
+      perspective: 0.1,
+      zoom: 0.5,
     };
 
     this.onPositionInput = this.onPositionInput.bind(this);
+    this.onPerspectiveInput = this.onPerspectiveInput.bind(this);
+    this.onZoomInput = this.onZoomInput.bind(this);
     this.updateImageDownloadDebounced = debounce(this.updateImageDownload, 200);
   }
 
@@ -96,7 +100,8 @@ export default class TiltShiftGenerator extends Component {
       round(this.state.position * height), 20, round(height/2)
     );
 
-    const delta = round(0.1 * width);
+    const delta = round(this.state.perspective * width);
+    const zoom = this.state.zoom;
     this.canvas.perspective(
       [
         ...[0, 0],
@@ -105,10 +110,10 @@ export default class TiltShiftGenerator extends Component {
         ...[width, height],
       ],
       [
-        ...[delta, 0],
-        ...[width - delta, 0],
-        ...[-delta, height],
-        ...[width + delta, height],
+        ...[delta - round(zoom * delta), 0],
+        ...[width - delta + round(zoom * delta), 0],
+        ...[-round(zoom * delta), height],
+        ...[width + round(zoom * delta), height],
       ],
     );
     this.canvas.vignette(0.5, 0.2);
@@ -118,6 +123,16 @@ export default class TiltShiftGenerator extends Component {
 
   onPositionInput(e) {
     this.setState({ position: e.detail.value / 100 });
+    this.updateCanvas();
+  }
+
+  onPerspectiveInput(e) {
+    this.setState({ perspective: e.detail.value / 100 });
+    this.updateCanvas();
+  }
+
+  onZoomInput(e) {
+    this.setState({ zoom: e.detail.value / 100 });
     this.updateCanvas();
   }
 
@@ -147,6 +162,36 @@ export default class TiltShiftGenerator extends Component {
             disabled={!this.state.image}
             discrete
             className="tilt-shift-generator-position__slider"
+          />
+        </div>
+        <div className="tilt-shift-generator-perspective">
+          <span className="tilt-shift-generator-perspective__label">
+            Perspective ({round(100 * this.state.perspective)}%)
+          </span>
+          <Slider
+            min={0}
+            max={100}
+            step={1}
+            value={100 * this.state.perspective}
+            onInput={this.onPerspectiveInput}
+            disabled={!this.state.image}
+            discrete
+            className="tilt-shift-generator-perspective__slider"
+          />
+        </div>
+        <div className="tilt-shift-generator-zoom">
+          <span className="tilt-shift-generator-zoom__label">
+            Zoom<sup>beta</sup> ({round(100 * this.state.zoom)}%)
+          </span>
+          <Slider
+            min={0}
+            max={100}
+            step={1}
+            value={100 * this.state.zoom}
+            onInput={this.onZoomInput}
+            disabled={!this.state.image}
+            discrete
+            className="tilt-shift-generator-zoom__slider"
           />
         </div>
         <Dropzone
