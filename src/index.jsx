d056fed670fee0dcbe94d0c59efd993240010eae
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-06 14:19:48 +0200

    Fixed ESLint/airbnb errors in src/index.{js => jsx}

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'material-components-web/material-components-web.scss';
+import 'material-components-web/material-components-web.scss'; // eslint-disable-line import/no-extraneous-dependencies
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
