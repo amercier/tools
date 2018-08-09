93380d7b1c92e1d989ab4f42800a803978d0fbeb
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-09 23:44:16 +0200

    Added minimal tests for TiltShiftGenerator/* components

---
+++ src/components/TiltShiftGenerator/DroppableImage.spec.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DroppableImage from './DroppableImage';
+import { noop } from '../common/lang';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<DroppableImage placeholder="" onDrop={noop} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
---
+++ src/components/TiltShiftGenerator/index.spec.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TiltShiftGenerator from '.';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<TiltShiftGenerator />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
