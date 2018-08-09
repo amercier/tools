546d10b3559f8ba9ac72cd59861ee2fb52cbbc02
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-09 23:33:36 +0200

    Added minimal tests for PasswordGenerator/* components

---
+++ src/components/PasswordGenerator/View.spec.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import View from './View';
+import { noop } from '../common/lang';
+
+const Noop = () => <div />;
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render((
+    <View
+      RenderOptions={Noop}
+      RenderPassword={Noop}
+      RenderCopyToClipboard={Noop}
+      password=""
+      updatePassword={noop}
+      onCopy={noop}
+      copied
+      length={32}
+      numbers
+      symbols
+      uppercase
+      excludeSimilarCharacters
+      onOptionChange={noop}
+    />
+  ), div);
+  ReactDOM.unmountComponentAtNode(div);
+});
