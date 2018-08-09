f585f32b02a04140800ea98f8a6979bfb805982f
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-09 23:46:11 +0200

    Added minimal tests for TravisConfigValidator/* components

---
+++ src/components/TravisConfigValidator/Log.spec.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Log from './Log';
+
+const Noop = () => <div />;
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Log RenderEntry={Noop} messages={[]} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
