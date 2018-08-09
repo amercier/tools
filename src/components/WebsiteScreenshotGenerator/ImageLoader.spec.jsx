2e366188085cd7385250295e3d88864317f08dc6
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-09 23:56:27 +0200

    Added minimal tests for WebsiteScreenshotGenerator/* components

---
+++ src/components/WebsiteScreenshotGenerator/ImageLoader.spec.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ImageLoader from './ImageLoader';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ImageLoader width={0} height={0} loading />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
