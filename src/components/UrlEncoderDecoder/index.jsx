c03856df1d9bdcf9259a57bc178b314b38eb203c
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-10 09:14:12 +0200

    Refactored {Base64,Url}EncoderDecoder to a pseudo-abstract class

--- src/components/UrlEncoderDecoder/index.jsx
+++ src/components/UrlEncoderDecoder/index.jsx
@@ -1,49 +1,14 @@
-import React, { Component } from 'react';
 import { urlEncode, urlDecode } from './url';
+import AbstactEncoderDecoder from '../common/EncoderDecoder/Abstract';
 
-import EncoderDecoderView from '../common/EncoderDecoderView';
+export default class Base64EncoderDecoder extends AbstactEncoderDecoder {
+  static encode = urlEncode;
 
-export default class Base64EncoderDecoder extends Component {
-  state = {
-    input: '',
-  };
-
-  onInputChange = ({ target }) => {
-    this.setState({ input: target.value });
-  };
-
-  get isEncodeDisabled() {
-    const { input } = this.state;
-    return input === '';
-  }
+  static decode = urlDecode;
 
   get isDecodeDisabled() {
     const { input } = this.state;
     const decoded = urlDecode(input);
     return input === '' || decoded === null || decoded === input;
-  }
-
-  encode = () => {
-    const { input } = this.state;
-    this.setState({ input: urlEncode(input) });
-  }
-
-  decode = () => {
-    const { input } = this.state;
-    this.setState({ input: urlDecode(input) });
-  }
-
-  render() {
-    return (
-      <EncoderDecoderView
-        {...this.props}
-        {...this.state}
-        onInputChange={this.onInputChange}
-        onEncode={this.encode}
-        onDecode={this.decode}
-        isEncodeDisabled={this.isEncodeDisabled}
-        isDecodeDisabled={this.isDecodeDisabled}
-      />
-    );
   }
 }
