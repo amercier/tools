c51b29e91e15e7c1e33dad444c7ec33c5e8fe6c9
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-10 14:47:40 +0200

    Renamed components/ views/

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'material-components-web/material-components-web.scss'; // eslint-disable-line import/no-extraneous-dependencies
-import App from './components/App';
+import App from './views/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.scss';
