0d41585adb317b4734453e97db77ca0f1e4ad9b7
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-19 22:11:13 +0200

    Ported WebsiteScreenshotGenerator to Material UI

--- src/views/WebsiteScreenshotGenerator/Toolbar.jsx
+++ src/views/WebsiteScreenshotGenerator/Toolbar.jsx
@@ -1,66 +1,93 @@
-import isUrl from 'is-url';
 import React from 'react';
-import { Button } from 'rmwc/Button';
-import { Select } from 'rmwc/Select';
-import { TextField } from 'rmwc/TextField';
-import { string, func, arrayOf } from 'prop-types';
+import { string, func, object, arrayOf } from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
+import isUrl from 'is-url';
 import { eventTargetProperty } from '../../lib/dom';
+
+const valueToMenuItem = (value, i) => (
+  <MenuItem key={`menu-item-${i}`} value={value}>
+    {value}
+  </MenuItem>
+);
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'flex-end',
+  },
+  group1: {
+    flexGrow: 1000,
+    margin: '0.5rem',
+    minWidth: '18em',
+  },
+  group2: {
+    flexGrow: 1,
+    display: 'flex',
+    margin: '0.5rem',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    marginLeft: '1rem',
+  },
+};
 
 const Toolbar = ({
   url, resolution, resolutions, blobUrl, filename,
   onUrlChange, onResolutionChange, onGo,
-}) => {
-  const styles = {
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      alignItems: 'flex-end',
-    },
-    group1: {
-      flexGrow: 1000,
-      margin: '0.5rem',
-      minWidth: '18em',
-    },
-    group2: {
-      flexGrow: 1,
-      display: 'flex',
-      margin: '0.5rem',
-      alignItems: 'flex-end',
-      justifyContent: 'space-between',
-    },
-    buttons: {
-      marginLeft: '1rem',
-    },
-  };
-  return (
-    <div className="website-screenshot-toolbar" style={styles.container}>
-      <div style={styles.group1}>
-        <TextField label="Website URL" onChange={eventTargetProperty(onUrlChange)} value={url} className="mdc-text-field--grow" />
-      </div>
+  classes,
+}) => (
+  <div className={classes.container}>
+    <div className={classes.group1}>
+      <TextField
+        label="Website URL"
+        onChange={eventTargetProperty(onUrlChange)}
+        value={url}
+        className={classes.url}
+        fullWidth
+      />
+    </div>
 
-      <div style={styles.group2}>
-        <div>
-          <Select
-            value={resolution}
-            options={resolutions.map(value => ({ value, label: value }))}
-            onChange={eventTargetProperty(onResolutionChange)}
-            label="Resolution"
-          />
-        </div>
-        <div>
-          <Button raised disabled={!isUrl(url)} onClick={onGo} style={styles.buttons}>
-            Go
+    <div className={classes.group2}>
+      <FormControl>
+        <InputLabel htmlFor="resolution-helper">
+          Resolution
+        </InputLabel>
+        <Select
+          value={resolution}
+          onChange={eventTargetProperty(onResolutionChange)}
+          input={<Input name="resolution" id="resolution-helper" />}
+        >
+          {resolutions.map(valueToMenuItem)}
+        </Select>
+      </FormControl>
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={!isUrl(url)}
+          onClick={onGo}
+          className={classes.buttons}
+        >
+          Go
+        </Button>
+        <a href={blobUrl} download={filename} className={classes.buttons}>
+          <Button disabled={!blobUrl}>
+            Download
           </Button>
-          <a href={blobUrl} download={filename} style={styles.buttons}>
-            <Button disabled={!blobUrl}>
-              Download
-            </Button>
-          </a>
-        </div>
+        </a>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 Toolbar.propTypes = {
   url: string.isRequired,
@@ -71,6 +98,7 @@ Toolbar.propTypes = {
   onUrlChange: func.isRequired,
   onResolutionChange: func.isRequired,
   onGo: func.isRequired,
+  classes: object.isRequired, // eslint-disable-line react/forbid-prop-types
 };
 
 Toolbar.defaultProps = {
@@ -78,4 +106,4 @@ Toolbar.defaultProps = {
   filename: null,
 };
 
-export default Toolbar;
+export default withStyles(styles)(Toolbar);
