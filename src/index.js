7d5825015b5e82dc314f3ecdc5bceefed901dcff
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-11-11 08:51:19 +0100

    Added @material-ui/styles dependency

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+/* eslint-disable */
+
+// @material-ui/styles
+// https://material-ui.com/css-in-js/basics/#migration-for--material-ui-core-users
+// TODO Cleanup after Material UI v4 upgrade
+import { install } from '@material-ui/styles';
+install();
+
 // Workaround for https://github.com/facebook/create-react-app/issues/3052
 import './index.jsx';
